9c00792fc11e92c20497e444db5e5ce0d7786a83
JUNIOR\main <wandernei@gmail.com>
2017-05-18 20:48:28 -0300

    Atualiza MySQL

---
+++ header.php
@@ -0,0 +1,31 @@
+        <div style="text-align: left;">
+            <img src="Resorces/Logo.jpg" alt="Logo" style="width:10%;height:10%;">
+            
+        </div>
+        <div class=bs-example data-example-id=default-navbar>
+            <nav class="navbar navbar-inverse">
+                <div class=container-fluid>
+                    <div class=navbar-header>
+
+                        <div class="collapse navbar-collapse" id=bs-example-navbar-collapse-1>
+                            <ul class="nav navbar-nav">
+                                <li class=dropdown> <a href=# class=dropdown-toggle data-toggle=dropdown role=button aria-haspopup=true aria-expanded=false>Cadastrar <span class=caret></span></a>
+                                    <ul class=dropdown-menu>
+                                        <li><a href=#>Transportadora</a></li>
+                                        <li><a href=#>Lote</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                            <ul class="nav navbar-nav navbar-right">
+                                <li class=dropdown> <a href=# class=dropdown-toggle data-toggle=dropdown role=button aria-haspopup=true aria-expanded=false>Entregas <span class=caret></span></a>
+                                    <ul class=dropdown-menu>
+                                        <li><a href=#>Consultar</a></li>
+                                        <li><a href=#>Cadastrar</a></li>
+                                        <li><a href=#>Cancelar</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+            </nav>
+        </div>
--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
+<html lang="en">
     <head>
-        <meta charset="ISO-8859-1">
-        <title></title>
+        <title>Sistema LogiMed<</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
     <body>
+        <?php include 'header.php';?>
+        <h4>Bem Vindo</h4>
+
         <?php
-        // put your code here
         ?>
     </body>
 </html>
