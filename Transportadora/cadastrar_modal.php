a0094e7a7b26bbcf93fc2b2cfc35b4a14bd2f278
JUNIOR\main <wandernei@gmail.com>
2017-05-25 19:53:41 -0300

    Atualiza Transportadoras CRUD e index.php

--- Transportadora/cadastrar_modal.php
+++ Transportadora/cadastrar_modal.php
@@ -1,71 +1,66 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>Modal Example</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!--BOOTSTRAP -->
-        <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 	   	
-     
-        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.15/css/dataTables.bootstrap.min.css">
 
-        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-        <script src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script> 
-        <script src="https://cdn.datatables.net/1.10.15/js/dataTables.bootstrap.min.js"></script> 
-    </head>
-    <body>
-        <div class="container">
+<head>
+    <title>Modal Example</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--BOOTSTRAP -->
+    <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.15/css/dataTables.bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.15/js/dataTables.bootstrap.min.js"></script>
+    <link rel="stylesheet" href="modal.css">
+</head>
 
-                <!-- Modal-->
-                <div class="modal fade" id="cadastrar" role="dialog">
-                    <div class="modal-dialog modal-sm">
-                        <!-- Content STARTS HERE-->
-                        <div class="modal-content">
-                            <div class="modal-header">
-								<button type="button" class="close" data-dismiss="modal">&times;</button>
-                                <h4 class="modal-title">Cadastrar Transportadora</h4>
+<body>
+    <div class="container">
+        <!-- Modal-->
+        <div class="modal fade" id="cadastrar" role="dialog">
+            <div class="modal-dialog modal-sm">
+                <!-- Content STARTS HERE-->
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">Cadastrar Transportadora</h4>
+                    </div>
+                    <div class="modal-body">
+
+                        <form role="form">
+                            <div class="form-group">
+                                <label for="nome">Transportadora:</label> <span class="glyphicon glyphicon-user"></span>
+                                <input type="text" class="form-control" id="nome" placeholder="Digite o nome" name="nome">
                             </div>
-                            <div class="modal-body">
-                                    <form class="form-" action="/action_page.php">
-
-                                        <div class="form-group col-sm-12">
-                                            <label for="nome">Transportadora:</label>
-                                            <input type="text" class="form-control" id="nome" placeholder="Digite o nome" name="nome">
-                                        </div>
-
-                                        <div class="form-group col-sm-12">
-                                            <label for="preco">PreÃ§o:</label>
-                                            <input type="text" class="form-control" id="preco" placeholder="Digite o preÃ§o" name="preco">
-                                        </div>
-
-                                        <div class="form-group col-sm-12">
-                                            <label for="avaliacao">AvaliaÃ§Ã£o:</label>
-                                            <input type="text" class="form-control" id="avaliacao" placeholder="Digite o avaliaÃ§Ã£o" name="avaliacao">
-                                        </div>
-
-                                        <div class="form-group col-sm-12">
-                                            <label for="zona_atendimento">Zona de Atendimento:</label>
-                                            <input type="text" class="form-control" id="zona_atendimento" placeholder="Digite a Zona de Atendimento" name="zona_atendimento">
-                                        </div>
-
-                                        <div class="checkbox">
-                                            <label><input type="checkbox" name="ativo" checked>Ativo?</label>
-                                        </div>
-
-                                        <button type="submit" class="btn btn-success">Salvar</button>
-                                    </form>					
-
-								<div class="modal-footer">
-                                  <!--  <button type="button" class="btn btn-default" data-dismiss="modal">&times;</button> -->
-                                </div>
+                            <div class="form-group">
+                                <label for="preco">Preço:</label>
+                                <input type="text" class="form-control" id="preco" placeholder="Digite o preço" name="preco">
+                            </div>
+                            <div class="form-group">
+                                <label for="avaliacao">Avaliação:</label>
+                                <input type="text" class="form-control" id="avaliacao" placeholder="Digite o Avaliação" name="avaliacao">
+                            </div>
+                            <div class="form-group">
+                                <label for="zona_atendimento">Zona de Atendimento:</label>
+                                <input type="text" class="form-control" id="zona_atendimento" placeholder="Digite a Zona de Atendimento" name="zona_atendimento">
+                            </div>
+                            <div class="checkbox">
+                                <label><input type="checkbox" name="ativo" value="" checked>Ativo?</label>
                             </div>
 
-                        </div>
-                    </div><!-- Modal ENDS HERE -->
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-success pull-left"><span class="glyphicon glyphicon-off"></span> Salvar</button>
+                        <button type="submit" class="btn btn-danger btn-default pull-right" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancelar</button>
+                    </div>
+
                 </div>
-
+            </div>
+            <!-- Modal ENDS HERE -->
         </div>
-    </body>
-</html>
+    </div>
+</body>
 
+</html>
--- Transportadora/editar_modal.php
+++ Transportadora/editar_modal.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
     <head>
         <title>Modal Example</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!--BOOTSTRAP -->
         <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 	   	
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <!--DATATABLE -->
         <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.15/css/dataTables.bootstrap.min.css">
-
         <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-        <script src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script> 
-        <script src="https://cdn.datatables.net/1.10.15/js/dataTables.bootstrap.min.js"></script> 
+        <script src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.10.15/js/dataTables.bootstrap.min.js"></script>
+        <link rel="stylesheet" href="modal.css">
+
     </head>
+
     <body>
         <div class="container">
             <!-- Modal-->
@@ -26,47 +29,44 @@
                             <h4 class="modal-title">Editar Transportadora</h4>
                         </div>
                         <div class="modal-body">
-                                <form class="form" action="/action_page.php">
+                            <form class="form" action="/action_page.php">
 
-                                    <div class="form-group col-sm-12">
-                                        <label for="nome">Transportadora:</label>
-                                        <input type="text" class="form-control" id="nome" placeholder="Digite o nome" name="nome">
-                                    </div>
+                                <div class="form-group">
+                                    <label for="nome">Transportadora:</label>
+                                    <input type="text" class="form-control" id="nome" placeholder="Digite o nome" name="nome">
+                                </div>
 
-                                    <div class="form-group col-sm-12">
-                                        <label for="preco">PreÃ§o:</label>
-                                        <input type="text" class="form-control" id="preco" placeholder="Digite o preÃ§o" name="preco">
-                                    </div>
+                                <div class="form-group">
+                                    <label for="preco">preço:</label>
+                                    <input type="text" class="form-control" id="preco" placeholder="Digite o preço" name="preco">
+                                </div>
 
-                                    <div class="form-group col-sm-12">
-                                        <label for="avaliacao">AvaliaÃ§Ã£o:</label>
-                                        <input type="text" class="form-control" id="avaliacao" placeholder="Digite o avaliaÃ§Ã£o" name="avaliacao">
-                                    </div>
+                                <div class="form-group">
+                                    <label for="avaliacao">Avaliação:</label>
+                                    <input type="text" class="form-control" id="avaliacao" placeholder="Digite o avaliação" name="avaliacao">
+                                </div>
 
-                                    <div class="form-group col-sm-12">
-                                        <label for="zona_atendimento">Zona de Atendimento:</label>
-                                        <input type="text" class="form-control" id="zona_atendimento" placeholder="Digite a Zona de Atendimento" name="zona_atendimento">
-                                    </div>
+                                <div class="form-group">
+                                    <label for="zona_atendimento">Zona de Atendimento:</label>
+                                    <input type="text" class="form-control" id="zona_atendimento" placeholder="Digite a Zona de Atendimento" name="zona_atendimento">
+                                </div>
 
-                                    <div class="checkbox">
-                                        <label><input type="checkbox" name="ativo" checked>Ativo?</label>
-                                    </div>
-
-                                    <br>
-
-                                    <button type="submit" class="btn btn-warning">Salvar</button>
-
-								</form>
-                            </div>
-                            <div class="modal-footer">
-                              <!--  <button type="button" class="btn btn-default" data-dismiss="modal">&times;</button> -->
-                            </div>
+                                <div class="checkbox">
+                                    <label><input type="checkbox" name="ativo" checked>Ativo?</label>
+                                </div>
+                            </form>
                         </div>
-
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-warning pull-left"><span class="glyphicon glyphicon-off"></span> Salvar</button>
+                            <button type="submit" class="btn btn-danger btn-default pull-right" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancelar</button>
+                        </div>
                     </div>
-                </div><!-- Modal ENDS HERE -->
-            </div>
-        </div>
-    </body>
-</html>
 
+                </div>
+            </div>
+            <!-- Modal ENDS HERE -->
+        </div>
+    </div>
+</body>
+
+</html>
